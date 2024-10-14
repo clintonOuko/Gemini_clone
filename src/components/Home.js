b8aa0e6085b017a8eeac34ecc37b642ae51b968a
clintonOuko <oukoclin@gmail.com>
2024-10-14 20:15:39 +0300

    gemini clone poject

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Gemini Clone by @infinite</h1>
+      </div>
+    )
+  }
+}
+
+export default Home
